4b21609a723183a3e8da05b0564ddab742996ffa
Fillipinh0 <128330975+Fillipinh0@users.noreply.github.com>
2024-07-03 00:03:39 -0300

    Deu um errinho no commit anterior ent estou mandando dnv

--- alfajor/arqui.py
+++ alfajor/arqui.py
@@ -1,26 +1,5 @@
 ### Calcula o que compras para efetuar o pagamaneto para Lu
 def compras():
-<<<<<<< HEAD
-    comprou = input("Vocês compraram algo esta semana?? S ou N")
-    if comprou == "S":
-        #lu = divid     <--- Para terminar o projeto preciso fazer o calculo da semana para ter o divid da semana e o banco de dados ainda  
-        quem_comprou = input("Ce voce comprou algo digite: s")
-        match quem_comprou:
-            case "s":
-                lu_val = 0
-                while True:
-                    repet = input("Digite 0 para continuar ou 1 se a Lu comprou!")
-                    if repet == "0":
-                        compras = input("Valor da compra:")
-                        lu_val = lu_val - (compras / 2)
-                    elif  repet == "1":
-                        compras = input("Valor da compra:")
-                        lu_val = lu_val + (compras / 2)
-                    else:
-                            print("Voce precisa pagar a Lu: ", lu_val)
-    else:
-         pass
-=======
     comprou = input("Vocês compraram algo esta semana?? S ou N ").upper()
     if comprou == "S":
         #Preciso aprender a mecher com banco de dados para passar o valor da semana para realemente saber quanto pagar... ate entao vou fazer semi manual
@@ -40,4 +19,3 @@ def compras():
          pass
     semana = input('Escreva o tal da semana: ')
 compras()
->>>>>>> 8197b75 (Mudei algumas cisas depois que usei ele...)
