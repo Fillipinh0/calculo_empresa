b1c736ce9c0016ff05e70c7c8b83fb2ee01c38b4
Fillipinh0 <128330975+Fillipinh0@users.noreply.github.com>
2024-07-02 23:58:28 -0300

    Alterei algumas coisas apos usar...

--- alfajor/arqui.py
+++ alfajor/arqui.py
@@ -1,5 +1,6 @@
 ### Calcula o que compras para efetuar o pagamaneto para Lu
 def compras():
+<<<<<<< HEAD
     comprou = input("Vocês compraram algo esta semana?? S ou N")
     if comprou == "S":
         #lu = divid     <--- Para terminar o projeto preciso fazer o calculo da semana para ter o divid da semana e o banco de dados ainda  
@@ -19,3 +20,24 @@ def compras():
                             print("Voce precisa pagar a Lu: ", lu_val)
     else:
          pass
+=======
+    comprou = input("Vocês compraram algo esta semana?? S ou N ").upper()
+    if comprou == "S":
+        #Preciso aprender a mecher com banco de dados para passar o valor da semana para realemente saber quanto pagar... ate entao vou fazer semi manual
+        lu_val = float(input("Escreva o valor total da semana: ")) / 2
+        while True:
+            repet = input("Digite 0 para continuar ou 1 se a Lu comprou!")
+            if repet == "0":
+                compras = float(input("Valor da compra:"))
+                lu_val = lu_val - (compras / 2)
+            elif  repet == "1":
+                compras = float(input("Valor da compra:"))
+                lu_val = lu_val + (compras / 2)
+            else:
+                print("Voce precisa pagar a Lu: ", lu_val)
+                break
+    else:
+         pass
+    semana = input('Escreva o tal da semana: ')
+compras()
+>>>>>>> 8197b75 (Mudei algumas cisas depois que usei ele...)
--- alfajor/calcula_dia.py
+++ alfajor/calcula_dia.py
@@ -3,11 +3,31 @@ def cal_pix_dia():
     dia = 0
 
     while True:
+<<<<<<< HEAD
         compra = input("Digite o numero ou 'n' para sair: ")
         if compra == "n":
             print(dia)
             break
+=======
+        compra = input("Voce pode:\n-Digitar um numero para somar\n-Digitar'n' para sair\n-Escrever 'delet' para deletar\n Digite: ")
+        if compra == "n":
+            print(dia)
+            break
+        elif compra == 'delet':
+            certe = input("Voce realmente quer deletar?")
+            if certe == 'deletar':
+                dia = 0
+>>>>>>> 8197b75 (Mudei algumas cisas depois que usei ele...)
         else:
             compra = float(compra)
             if compra > -100:
                 dia += compra
+<<<<<<< HEAD
+=======
+    return(dia)
+valor_semana = []
+valor_dia = cal_pix_dia()
+print(valor_dia)
+valor_semana.push(valor_dia)
+print(valor_semana)
+>>>>>>> 8197b75 (Mudei algumas cisas depois que usei ele...)
