d7636cd6c260c7a52c72cdac48fdf2e80e65887b
Fillipinh0 <128330975+Fillipinh0@users.noreply.github.com>
2024-07-11 14:47:41 -0300

    alterei algumas coisa apos o uso e troquei o if por match

--- alfajor/arqui.py
+++ alfajor/arqui.py
@@ -1,21 +1,25 @@
 ### Calcula o que compras para efetuar o pagamaneto para Lu
 def compras():
-    comprou = input("Vocês compraram algo esta semana?? S ou N ").upper()
+    comprou = input("Vocês compraram algo esta semana?? \n-Digite 'S' ou 'N' \nDigite: ").upper()
     if comprou == "S":
         #Preciso aprender a mecher com banco de dados para passar o valor da semana para realemente saber quanto pagar... ate entao vou fazer semi manual
         lu_val = float(input("Escreva o valor total da semana: ")) / 2
         while True:
-            repet = input("Digite 0 para continuar ou 1 se a Lu comprou!")
-            if repet == "0":
-                compras = float(input("Valor da compra:"))
-                lu_val = lu_val - (compras / 2)
-            elif  repet == "1":
-                compras = float(input("Valor da compra:"))
-                lu_val = lu_val + (compras / 2)
-            else:
-                print("Voce precisa pagar a Lu: ", lu_val)
-                break
+            repet = input("-Se voce comprou tecle '0' \n-se a Lu comprou tecle '1' \n-passar para etapa 2 tecle '2' \nDigite:")
+            match repet:
+                case '0':
+                    compras = float(input("Valor da compra:"))
+                    lu_val = lu_val - (compras / 2)
+                case '1':
+                    compras = float(input("Valor da compra:"))
+                    lu_val = lu_val + (compras / 2)
+                case '2':
+                    valor_dinheiro = float(input("Digite total em dinheiro fisico:"))
+                    pagar_lu = (lu_val - valor_dinheiro)
+                    print("Voce precisa pagar a Lu: ", pagar_lu, 'total dela',lu_val)
+                    break
     else:
-         pass
-    semana = input('Escreva o tal da semana: ')
+        lu_val = float(input("Escreva o valor total da semana: ")) / 2
+        print("Voce precisa pagar a Lu: ", pagar_lu, 'total dela',lu_val)
+
 compras()
